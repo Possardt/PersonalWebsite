afd78f4ad5827b6c352926bb20ef3b51ab362bac
Ryan Possardt <rpossardt@gmail.com>
2017-02-28 21:23:51 -0500

    made small and md screen displays

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+	//scroll function listener for nav bar
+	$(window).on('scroll', function(){
+		if(screen.width <= 575){
+			var scroll = $(window).scrollTop();
+			var headerHeight = $('.headshot-container').height();	
+
+			if(scroll > headerHeight){
+				$('#name').addClass('scrollBelowHeader');
+				$('#name-placeholder').show();
+			}else {
+				$('#name').removeClass('scrollBelowHeader');
+				$('#name-placeholder').hide();
+			}
+		}else{
+			var scroll = $(window).scrollTop();
+			var navHeight = $('.headshot-container').height() + 60;	
+
+			if(scroll > navHeight){
+				$('.nav-container-small').addClass('scrollBelowHeader');
+				$('.nav-placeholder').show();
+			}else {
+				$('.nav-container-small').removeClass('scrollBelowHeader');
+				$('.nav-placeholder').hide();
+			}
+		}
+	});
+});

11b2d3304d8f9e2bf24f221e848df33b54c62659
Ryan Possardt <rpossardt@gmail.com>
2017-04-09 16:02:43 -0400

    Fixed bug with scrolling on resume page

--- scripts/resume.js
+++ scripts/resume.js
@@ -1,13 +1,17 @@
 $(document).ready(function(){
+	var isScrollNavVisible = false;
 
 	//scroll menu when 
 	$(window).on('scroll',function(){
 		var scroll = $(window).scrollTop();
 		var scrollNav = $('.scroll-nav');
-		if(scrollNav.position().top < 0 && scroll > $('.name').position().top){
+		if(!isScrollNavVisible && scrollNav.position().top < 0 && scroll > $('.name').position().top){
 			scrollNav.animate({top : 0}, 200);
-		}else if(scroll < $('.name').position().top && scrollNav.position().top >= 0){
+			isScrollNavVisible = true;
+		}else if(isScrollNavVisible && scroll < $('.name').position().top && scrollNav.position().top >= 0){
 			scrollNav.animate({top : -90}, 200);
+			isScrollNavVisible = false;
+			console.log('hopefully don\'t call this a fuckton of times');
 		}
 
 	});
